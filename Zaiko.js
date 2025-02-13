b358732014539a862ddee61f2171e32053f7f3ce
Dove <151492415+DesignerDeveloperDove@users.noreply.github.com>
2025-02-12 21:09:40 -0500

    Object Test

--- Zaiko.js
+++ Zaiko.js
@@ -1,2 +1,9 @@
 console.log("The JS sheet is Connected");
 // The Goal is to create an object that displays on the left hand side as a side bar that has key value pairs in it
+
+Sidebar = {
+    ProdName: "BurgerPatty",
+    Location: "Walk-In",
+    Qty: 50
+};
+console.log(Sidebar)

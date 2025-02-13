a14e0ebc75241c792b269b4f4e6c1b7c3a14b8fa
DesignerDeveloperDove <dove51903@gmail.com>
2025-02-12 22:07:42 -0500

    Product

--- Zaiko.js
+++ Zaiko.js
@@ -2,11 +2,11 @@ console.log("The JS sheet is Connected");
 // The Goal is to create an object that displays on the left hand side as a side bar that has key value pairs in it
 
 
-Sidebar = {
+Product = {
     ProdName: "BurgerPatty",
     Location: "Walk-In",
     Qty: 50
 };
 console.log(Sidebar)
 
-document.getElementById("Data").innerHTML = "Hello";
+document.getElementById("Data").innerHTML = Product.ProdName;

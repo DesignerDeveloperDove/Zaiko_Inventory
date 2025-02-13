4229479fc5059d1a65b69d14301f52e1efb02e5e
Dove <151492415+DesignerDeveloperDove@users.noreply.github.com>
2025-02-12 21:07:38 -0500

    Update

--- Zaiko.js
+++ Zaiko.js
@@ -1,2 +1,2 @@
 console.log("The JS sheet is Connected");
-// The Goal is to create an object that displays on the left hand side as a side bar that has key value pirs in it
+// The Goal is to create an object that displays on the left hand side as a side bar that has key value pairs in it

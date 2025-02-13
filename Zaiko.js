1eefb08dda8072a3aff4fe1ed003b07d83ad188c
DesignerDeveloperDove <dove51903@gmail.com>
2025-02-12 22:55:25 -0500

    Final for the day

--- Zaiko.js
+++ Zaiko.js
@@ -2,11 +2,52 @@ console.log("The JS sheet is Connected");
 // The Goal is to create an object that displays on the left hand side as a side bar that has key value pairs in it
 
 
-Product = {
-    ProdName: "BurgerPatty",
-    Location: "Walk-In",
-    Qty: 50
-};
-console.log(Sidebar)
 
-document.getElementById("Data").innerHTML = Product.ProdName;
+// im trying to make a function that creates an object
+// this object should take perameter 
+// those peraneter should be data 
+
+// This function is a function that creates a single object 
+function Objectifier(ProdName, Location, Qty){
+
+    return{
+        ProductName: ProdName,
+        ProductLocation: Location,
+        Quantitiy: Qty,
+    };
+
+}
+
+// this makes a new object
+let Product = Objectifier("Patty","Walk-In",25)
+
+//console.log(Product);
+
+//this displays the object
+function display(Name){
+    
+    document.getElementById("Data").innerHTML = 
+    Product.ProductName + " <br> " + Product.ProductLocation + " <br> " + Product.Quantitiy;
+    
+}
+display(Product)
+// creates an array that holds the Objects
+let Walk_In=[Product];
+console.log(Walk_In);
+
+
+// WANT TO CREATE A FUNCTION THAT CREATES A NEW OBJECT 
+function NewProduct(){
+    return{
+    let Product2 : Objectifier("Pickles","Walk-In",56)
+    }
+}
+display(Product2)
+
+//document.getElementById("Data").innerHTML = Product;
+
+
+
+/* This doesnt work, the reason is because you need to create a function that creates a new object using the objectefier object
+
+i recommend using a loop of some sort*/

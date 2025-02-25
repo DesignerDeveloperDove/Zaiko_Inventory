a4944d41b23dab2eb58dfb7bbf2e70f2ffd703e8
DesignerDeveloperDove <dove51903@gmail.com>
2025-02-24 20:23:20 -0500

    trial

--- Zaiko/vite.config.js
+++ Zaiko/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ["@swc/core-darwin-x64"],
+    },
+  },
+  optimizeDeps: {
+    exclude: ["@swc/core-darwin-x64"],
+  },
+});

ca19af6e71e98b8e9aa46de879b41ff438c81d4c
DesignerDeveloperDove <dove51903@gmail.com>
2025-02-24 21:34:45 -0500

    Pictures

--- Zaiko/src/Pages/LogIn.jsx
+++ Zaiko/src/Pages/LogIn.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import ZaikoLogo from './assets/ZaikoLogo.png'
 
 function LogIn(){
     return(
         <>
-        
-        
+        <div>
+      <img src={ZaikoLogo} alt="description" />
+    </div>
+        <h1>Log in here</h1>
+
         </>
     );
 
--- Zaiko/src/main.jsx
+++ Zaiko/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ProductCard from './Productcard.jsx'
+import LogIn from './Pages/LogIn.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <LogIn />
     <ProductCard />
   </StrictMode>,
 )

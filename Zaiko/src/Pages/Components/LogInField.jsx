cc20af94350c6f2e100af26550f3ec5f18082b86
ScottRzasa <arzasa@students.kennesaw.edu>
2025-02-26 17:13:02 -0500

    no space in placeholder

--- Zaiko/src/Pages/Components/LogInField.jsx
+++ Zaiko/src/Pages/Components/LogInField.jsx
@@ -6,8 +6,8 @@ function UserLogIn(){
         <>
         
         <form action="" id="LogInForm">
-            <input type="text" id="UserName" placeholder=" Username " />
-            <input type="text" id="Password" placeholder=" Password "/>
+            <input type="text" id="UserName" placeholder="Username" />
+            <input type="text" id="Password" placeholder="Password"/>
             <button id="LogInBtn"><h2>Submit</h2></button>
 
         </form>

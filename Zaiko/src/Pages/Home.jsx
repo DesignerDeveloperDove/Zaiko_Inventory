118f8bd272ade2663d8c4333ec2fa93b80c9f152
ScottRzasa <arzasa@students.kennesaw.edu>
2025-03-05 18:31:43 -0500

    walk-in button

--- Zaiko/src/Pages/Home.jsx
+++ Zaiko/src/Pages/Home.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Footer from "./Components/Footer";
 import DataInfo from "./Components/Data";
+import { Link } from "react-router";
 function Home(){
     return(
         <>
         <h2>Locations</h2>
         <div className="main column">
             <div className="row">
-                <div className="LocationBox">
+                <Link to={"/Walk-In"}>
+                <button className="LocationBox">
                     <h3>Walk-In</h3>
                     <p>10 Items</p>
-                    </div>
+                    </button>
+                    </Link>
                 <div className="LocationBox">
                     <h3>Deep Freezer</h3>
                     <p>10 Items</p>

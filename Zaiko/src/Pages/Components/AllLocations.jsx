4eaf0b5c2257edd9e2ea7475e7aa9d3a4a6da838
DesignerDeveloperDove <dove51903@gmail.com>
2025-03-19 22:35:05 -0400

    Locations

--- Zaiko/src/Pages/Components/AllLocations.jsx
+++ Zaiko/src/Pages/Components/AllLocations.jsx
@@ -35,9 +35,9 @@ function Los() {
 console.log(products);
     return (
         <>
-            
+                            <h1 className="Header">Locations</h1>
+
             <div className="LocationTabs">
-                <h1 className="Header">Locations</h1>
                 <ul>
                 {locationList.length > 0 ? (
                     locationList.map((location, index) => (

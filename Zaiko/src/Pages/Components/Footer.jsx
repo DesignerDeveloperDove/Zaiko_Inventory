0fc8c99b15c484b7d1073836e619439fa544df4c
ScottRzasa <arzasa@students.kennesaw.edu>
2025-03-24 20:11:04 -0400

    nothing changed

--- Zaiko/src/Pages/Components/Footer.jsx
+++ Zaiko/src/Pages/Components/Footer.jsx
@@ -5,13 +5,9 @@ function Footer(){
     return(
         <footer id="Footer">
             <div>
-                <h3>Test</h3>
-                <Link to="/home"></Link>
                 <img src="../src/assets/inventory icon.svg" alt="" />
                 <img src="../src/assets/profile icon.svg" alt="" />
                 <img src="../src/assets/reports icon.svg" alt="" />
-
-
             </div>
         </footer>
     )

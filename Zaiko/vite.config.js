13c8f3412a8dbd74b78f08e7869383781f118929
DesignerDeveloperDove <dove51903@gmail.com>
2025-04-07 23:46:26 -0400

    vite

--- Zaiko/vite.config.js
+++ Zaiko/vite.config.js
@@ -2,17 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  base: '/Zaiko/', // 👈 this matches your hosting path
+
   plugins: [react()],
+
   build: {
     rollupOptions: {
       external: ["@swc/core"],
     },
   },
+
   optimizeDeps: {
     exclude: ["@swc/core"],
   },
+
   define: {
     "process.env.SWC_USE_WASM": "true",
   },
-  
 });
